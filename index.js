d398c6137adfb6da7985ae03cfce80e010785851
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2017-01-29 22:38:25 +0530

    updating

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var dust = require('dust')();
 var serand = require('serand');
 var utils = require('utils');
 var Vehicle = require('vehicles-service');
-var list = require('vehicles-listing');
+var list = require('vehicles-find');
 
 module.exports = function (sandbox, fn, options) {
     Vehicle.find({
