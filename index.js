f437990f17e6deedddb5dcfc6ce3d8b3c94bf9aa
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2019-01-27 10:09:42 +0530

    updating

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
-var dust = require('dust')();
-var serand = require('serand');
-var utils = require('utils');
 var Vehicle = require('vehicles-service');
 var list = require('vehicles-find');
 
+var numbers = ['price', 'manufacturedAt'];
+
+var cast = function (field, val) {
+  if (numbers.indexOf(field) === -1) {
+      return val;
+  }
+  return parseInt(val, 10) || null;
+};
+
 module.exports = function (ctx, container, options, done) {
-    var sandbox = container.sandbox;
+    var query = options.query;
+    var o = {};
+    Object.keys(query).forEach(function (name) {
+        if (name.indexOf(':') === -1) {
+            return o[name] = cast(name, query[name]);
+        }
+        var parts = name.split(':');
+        var q = {};
+        var field = parts[0];
+        var operator = '$' + parts[1];
+        q[operator] = cast(field, query[name]);
+        o[field] = o[field] || (o[field] = {});
+        _.merge(o[field], q);
+    });
     Vehicle.find({
-        query: options,
+        query: o,
         images: '288x162'
     }, function (err, vehicles) {
         if (err) {
             return done(err);
         }
-        list(ctx, sandbox, {
+        list(ctx, container, {
+            deck: options.deck,
             vehicles: vehicles,
             size: 4
         }, done);
